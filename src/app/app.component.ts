666e613696aee77c770a2bfc16f243d7bad18b59
unknown <pinheiro_felipeg@yahoo.com.br>
2023-11-05 10:02:20 -0300

    Movendo codigo relacionado ao jogo para o modulo game

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,6 @@
 import { Component, HostListener, OnInit } from '@angular/core';
 
-import { GameObjectService } from './core/service/game-object.service';
-import { GameStatusEnum } from './core/enum/game-status.enum';
-
-import { CanvasUtil } from './shared/util/canvas.util';
 import { Control } from './shared/model/control';
-import { State } from './shared/model/state';
-import { EnemySpawner } from './shared/model/enemy-spawner';
 
 @Component({
   selector: 'app-root',
@@ -15,9 +9,6 @@ import { EnemySpawner } from './shared/model/enemy-spawner';
 })
 export class AppComponent implements OnInit {
   title = 'asteroid-ng';
-
-  private _state = State.getInstance();
-  private _enemySpawner = new EnemySpawner();
 
   // TODO: MOVER LOGICA PARA OUTRAS CLASSES
   @HostListener('window:keydown', ['$event'])
@@ -38,34 +29,7 @@ export class AppComponent implements OnInit {
     }
   }
 
-  constructor(private readonly gameObjectService: GameObjectService) {}
+  constructor() {}
 
-  ngOnInit(): void {
-    this._state.gameObjects = this.gameObjectService.getGameObjects();
-
-    this.runGame();
-  }
-
-  runGame(): void {
-    if (this._state.gameStatus != GameStatusEnum.RUNNING) {
-      return;
-    }
-
-    CanvasUtil.clearCanvas();
-
-    this._enemySpawner.spawnEnemies();
-
-    this._state.gameObjects.forEach((obj) => {
-      obj.shoot();
-      obj.move();
-      obj.collide();
-      obj.draw();
-    });
-
-    this._state.gameObjects = this._state.gameObjects.filter(
-      (obj) => obj.isAlive
-    );
-
-    requestAnimationFrame(() => this.runGame());
-  }
+  ngOnInit(): void {}
 }
--- src/app/modules/game/game.component.ts
+++ src/app/modules/game/game.component.ts
@@ -2,6 +2,11 @@ import { Component, OnInit } from '@angular/core';
 
 import { State } from './../../shared/model/state';
 import { Player } from './../../shared/model/player';
+import { EnemySpawner } from './../../shared/model/enemy-spawner';
+import { CanvasUtil } from './../../shared/util/canvas.util';
+
+import { GameObjectService } from './../../core/service/game-object.service';
+import { GameStatusEnum } from './../../core/enum/game-status.enum';
 
 @Component({
   selector: 'app-game',
@@ -11,10 +16,38 @@ import { Player } from './../../shared/model/player';
 export class GameComponent implements OnInit {
   private _state: State = State.getInstance();
   private _player: Player = Player.getInstance();
+  private _enemySpawner = new EnemySpawner();
 
-  constructor() {}
+  constructor(private readonly gameObjectService: GameObjectService) {}
 
-  ngOnInit() {}
+  ngOnInit() {
+    this._state.gameObjects = this.gameObjectService.getGameObjects();
+
+    this.runGame();
+  }
+
+  runGame(): void {
+    if (this._state.gameStatus != GameStatusEnum.RUNNING) {
+      return;
+    }
+
+    CanvasUtil.clearCanvas();
+
+    this._enemySpawner.spawnEnemies();
+
+    this._state.gameObjects.forEach((obj) => {
+      obj.shoot();
+      obj.move();
+      obj.collide();
+      obj.draw();
+    });
+
+    this._state.gameObjects = this._state.gameObjects.filter(
+      (obj) => obj.isAlive
+    );
+
+    requestAnimationFrame(() => this.runGame());
+  }
 
   public get score(): string {
     return this._state.score.toString().padStart(10, '0');
